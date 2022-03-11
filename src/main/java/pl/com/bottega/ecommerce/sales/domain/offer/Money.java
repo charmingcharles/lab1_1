387635f26ac9ad31c212e3ca73215e9f09c482fb
Karol Leśniewski <lesniewski.karol99@gmail.com>
2022-03-11 18:20:42 +0100

    stworzenie klesy Money

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -0,0 +1,26 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.math.BigDecimal;
+
+public class Money {
+
+    private BigDecimal amount;
+
+    private String currency;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
